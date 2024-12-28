5b033af75fa8f9bfe87be6ffdc7abe33884a5b47
GCB <gimelsonbalajadia@yahoo.com.ph>
2024-12-28 15:11:26 +0800

    initial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ServicesComponent } from './services/services.component';
+const routes: Routes = [
+  { path: '', component: HomeComponent }, // Home
+  { path: 'about', component: AboutComponent }, // About
+  { path: 'services', component: ServicesComponent }, // Services/Products
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
