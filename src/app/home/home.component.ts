555ce26ee94dd693d956e151abb029beb74d7c13
GCB <gimelsonbalajadia@yahoo.com.ph>
2024-12-31 20:23:56 +0800

    home and service update

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,13 +3,20 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  yearsInBusiness: number = 0;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.calculateYearsInBusiness();
   }
 
+  calculateYearsInBusiness(): void {
+    const currentYear = new Date().getFullYear();
+    const startYear = 2018;
+    this.yearsInBusiness = currentYear - startYear;
+  }
 }
